2cacd475cf4569102ce7188bb25abd9edbf48f6a
Aravinda Reddy Gangalakunta <38257404+aravind2060@users.noreply.github.com>
2024-03-19 02:51:02 +0000

    Avg of Tmax,  tmin for specific month grouped by station id, element type, and yyyymm

---
+++ notebooks/TemperatureAnalysis.py
@@ -0,0 +1,61 @@
+import sys
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, avg
+
+def initialize_spark(app_name,master_node_url):
+    """
+    Initialize and return a Spark session
+    """
+    return SparkSession.builder.appName(app_name).master(master_node_url).getOrCreate()
+
+def read_data(spark, input_file, schema):
+    """
+    Read and return data from the CSV file using the provided Spark session and schema
+    """
+    return spark.read.csv(input_file, header=False, schema=schema)
+
+def filter_temperature_data(df, year_month):
+    """
+    Filter data for the given month and temperature elements (TMAX, TMIN)
+    """
+    return df.filter(
+        (col("DATE").startswith(year_month)) &
+        (col("ELEMENT").isin("TMAX", "TMIN"))
+    )
+
+def calculate_average_temperatures(filtered_df):
+    """
+    Calculate and return the average temperatures
+    """
+    return filtered_df.groupBy("ID", "ELEMENT").agg(
+        avg("DATA_VALUE").alias("Avg_Temperature")
+    )
+
+def save_results(result_df, output_file):
+    """
+    Save the result DataFrame to the specified output file
+    """
+    result_df.write.csv(output_file, header=True)
+
+def main(master_node_url,year_month, input_file, output_file):
+    
+    spark = initialize_spark("Temperature Analysis",master_node_url)
+
+    schema = "ID STRING, DATE STRING, ELEMENT STRING, DATA_VALUE INT, M_FLAG STRING, Q_FLAG STRING, S_FLAG STRING, OBS_TIME STRING"
+    data_df = read_data(spark, input_file, schema)
+
+    temperature_df = filter_temperature_data(data_df, year_month)
+    avg_temperature_df = calculate_average_temperatures(temperature_df)
+
+    save_results(avg_temperature_df, output_file)
+
+    spark.stop()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python mtemp.py <year_month> <input_file> <output_file>")
+        sys.exit(-1)
+
+    year_month, input_file, output_file = sys.argv[1:4]
+    master_node_url="spark://spark-master:7077";
+    main(master_node_url,year_month, input_file, output_file)
